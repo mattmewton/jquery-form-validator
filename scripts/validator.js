9d7de337cec31ddfe9271f83d1a4edef915bcdaf
Matt Mewton <matt@readysetrocket.com>
2014-10-21 12:28:29 -0400

    Commenting out phone code for now

--- scripts/validator.js
+++ scripts/validator.js
@@ -55,7 +55,7 @@ if (typeof Object.create !== "function") {
       var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
       var ck_address = /^([0-9]{1,6})([\w\s]{3,128})$/;
       var ck_zip = /^[0-9]{5}$/;
-      var ck_phone = /^\(\d{3}\)\ \d{3}-\d{4}$/;
+      //var ck_phone = /^\(\d{3}\)\ \d{3}-\d{4}$/;
       //var ck_address = /^[0-9]{1,8}$/;
       //var ck_appt = /^[A-Za-z0-9 ]{0,128}$/;
       //var ck_phone = /^\d{3}-\d{3}-\d{4}$/;
